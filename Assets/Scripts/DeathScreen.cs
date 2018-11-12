2b8187c100623db1919558540abf9ba112ae6a3c
Furris <christopher.furoy@hotmail.com>
2018-11-12 12:30:51 +0100

    Added more dankness to deathscreen
    xd

--- Assets/Scripts/DeathScreen.cs
+++ Assets/Scripts/DeathScreen.cs
@@ -17,11 +17,18 @@ public class DeathScreen : MonoBehaviour {
             SceneManager.LoadScene("Level1");
             LevelManager.tacosCollected = 0;
         }
+        if( Input.GetKeyDown(KeyCode.M))
+        {
+            SceneManager.LoadScene("Menu");
+            LevelManager.tacosCollected = 0;
+        }
 
     }
 
     void OnGUI()
     {
+        string respawnText = "OOPS, YOU DIED! \n Press R to retry or M to get to the main menu.";
+        GUI.Box(new Rect(Screen.width - 685, 100, 300, 50), respawnText);        
         string tacoText = "Total tacos: " + LevelManager.tacosCollected;
         GUI.Box(new Rect(Screen.width - 600, 200, 130, 25), tacoText);
         string livesText = "Lives spent: " + LevelManager.lives;
