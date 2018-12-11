31f46754e2a12531b00c3da7c4ba670490031b94
Christopher Furøy <christopher.furoy@hotmail.com>
2018-12-11 14:21:17 +0100

    Revert "scoreboard"
    This reverts commit 80229220c2df2bdfd60c5fd7f3c8097bc7494e02.

--- Assets/Scripts/gamecontroller1.cs
+++ Assets/Scripts/gamecontroller1.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-public class gamecontroller1 : MonoBehaviour
-{
+public class gamecontroller1 : MonoBehaviour {
 
     public Text TacoCounter;
     public Text TimeUsed;
@@ -21,22 +20,20 @@ public class gamecontroller1 : MonoBehaviour
     public Button submit;
     public GameObject myButton;
     public GameObject myInput;
-    public GameObject myButton2;
-    //ikke endre
+    public GameObject myText;
 
 
     // Use this for initialization
-    void Start()
-    {
+    void Start () {
         Name.characterLimit = 7;
         Cursor.visible = true;
         taco = LevelManager.tacosCollected;
         death = LevelManager.lives;
-        time = LevelManager.timespend;
+        time = LevelManager.timespend; 
         timemath = Mathf.RoundToInt(LevelManager.time);
-        scoremath = ((taco * 100) / (timemath / 5));
+        scoremath = ((taco*100) / (timemath/5));
         score = Mathf.RoundToInt(scoremath);
-
+       
         if (taco == 0 || timemath == 0)
         {
             score = 0;
@@ -48,10 +45,9 @@ public class gamecontroller1 : MonoBehaviour
         DeathCount.text = "Number of deaths: " + death;
         Score.text = "Score: " + score;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
+	
+	// Update is called once per frame
+	void Update () {
     }
 
     public void BackToMenu()
@@ -67,24 +63,15 @@ public class gamecontroller1 : MonoBehaviour
             GetComponent<leaderboard1>().SubmitScore(Name.text, taco, death, time, score);
             myButton.SetActive(false);
             myInput.SetActive(false);
-
+            myText.SetActive(false);
         }
         if (currentScene.name == "Scoreboard Easy")
         {
             GetComponent<leaderboardEasy>().SubmitScore(Name.text, taco, death, time, score);
             myButton.SetActive(false);
             myInput.SetActive(false);
-
+            myText.SetActive(false);
         }
-
-    }
-
-    public void backToMenu()
-    {
-
-        SceneManager.LoadScene("Menu", LoadSceneMode.Single);
-
-
-
+        
     }
 }
